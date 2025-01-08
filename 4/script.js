6e0a974c4dd73f12cc82300148459566f50afb18
jyy009 <153378401+jyy009@users.noreply.github.com>
2025-01-07 17:17:37 -0800

    Add event handlers

--- 4/script.js
+++ 4/script.js
@@ -8,3 +8,18 @@ buttonContainer.addEventListener("click", (event) => {
 buttonChildContainer.addEventListener("click", (event) => {
   alert("Child container clicked!");
 });
+
+const button2 = document.getElementById("button2");
+button2.onclick = function () {
+  alert("Level 0 event handler");
+};
+
+const button3 = document.getElementById("button3");
+
+button3.addEventListener("click", () => {
+  alert("Level 2 event handler");
+});
+
+buttonChildContainer.addEventListener("click", (event) => {
+  console.log("Event delegation on:", event.target.textContent);
+});
